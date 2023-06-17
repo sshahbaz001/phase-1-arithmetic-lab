ca30c0a7d0461d6e6a14ddf0f7cf4af49072b0fd
Saba Shahbaz <sabashahbaz001@gmail.com>
2023-06-17 16:05:07 -0400

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2 
+let multiply = num1 * num2 
+
+const random = 35
+Math.random();
+
+const num3 = 14
+const num4 = 5
+
+let mod = num3 % num4 ;
+
+let max = 20
+ math.max(1,2,4,5,20);
+
